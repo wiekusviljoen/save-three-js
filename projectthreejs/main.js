da648df49df4f85c68b5ef8fa97d75e685999d90
wiekusviljoen <wiekusviljoen@gmail.com>
2022-08-31 15:04:08 +0200

    commit on master

--- projectthreejs/main.js
+++ projectthreejs/main.js
@@ -34,7 +34,13 @@ button5.onclick = function e() {
   x3.style.display = "none";
 };
 
-button6.onclick = function f() {};
+button6.onclick = function f() {
+  main.style.display = "grid";
+  button.innerText = "Focus on our home planet";
+  x2.style.display = "none";
+  x3.style.display = "none";
+  x4.style.display = "none";
+};
 
 //button2.onclick = function b() {
 //camera.position.z = 300;
