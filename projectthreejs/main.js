7cd04ad88948094de90a81b56d929212b9388850
wiekusviljoen <wiekusviljoen@gmail.com>
2022-08-31 22:36:59 +0200

    commit on master

--- projectthreejs/main.js
+++ projectthreejs/main.js
@@ -258,7 +258,7 @@ function addStar() {
   scene.add(star);
 }
 
-Array(300).fill().forEach(addStar);
+Array(150).fill().forEach(addStar);
 
 const spaceTexture = new THREE.TextureLoader().load("space.jpg");
 scene.background = spaceTexture;
